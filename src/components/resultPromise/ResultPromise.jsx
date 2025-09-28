42f13566933e621e60e83e280517a80cc83c05dd
himel2535 <monwarhossanhimel@gmail.com>
2025-09-29 03:10:38 +0600

    barchart with tooltip and legend

--- src/components/resultPromise/ResultPromise.jsx
+++ src/components/resultPromise/ResultPromise.jsx
@@ -1,5 +1,6 @@
 import React, { use } from "react";
 import ResultsChart from "../ResultsChart/ResultsChart";
+import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from "recharts";
 
 const ResultPromise = ({ resultData }) => {
   const resultsPromise = use(resultData);
@@ -23,8 +24,21 @@ const ResultPromise = ({ resultData }) => {
   console.log(marksChartData);
 
   return(
-    <div>
-        
+    <div className="flex justify-center items-center min-h-screen">
+        <BarChart width={1000} height={300} data={marksChartData}>
+
+
+            <Tooltip></Tooltip>
+            <Legend></Legend>
+            <XAxis dataKey={"name"}></XAxis>
+            <YAxis></YAxis>
+            <Bar dataKey={"avg"} fill="blue"></Bar>            
+            <Bar dataKey={"phy"} fill="green"></Bar>            
+            <Bar dataKey={"che"} fill="yellow"></Bar>            
+            <Bar dataKey={"bio"} fill="pink"></Bar>
+            <Bar dataKey={"math"} fill="red"></Bar>
+
+        </BarChart>
     </div>
   )
 };
